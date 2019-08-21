a633477f68ebdab95a1f83533546a56908db6ff7
Gabriel Soto <gabriel.soto6@upr.edu>
2019-08-21 12:13:55 -0400

    Added the class Timer to avoid cluttering and having a huge wrapper class, cleaned up some hard-coded numbers and organized most of the code.

--- ICOM4009_Timer/src/Main.java
+++ ICOM4009_Timer/src/Main.java
@@ -9,95 +9,29 @@ import javax.swing.JFrame;
 public class Main {
 
 	public static class Comp extends JComponent {
-
-		private long t = System.currentTimeMillis();
-		private long pT = 0;
-		private long pause_time = 0;
-		private String timer = "";
-		private String timerText = "";
-		private int mins = 0;
-		private long ms = 0;
-		private int s = 0;
-		private int hrs = 0;
-		private boolean pause = true;
+		
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		private String timer_text = "";
+		private Timer timer = null;
 		private Font bigFont = new Font("Arial", Font.BOLD, 30);
 		private Font smallFont = new Font("Arial", Font.BOLD, 14);
 		private JButton pause_b;
 		private JButton reset_b;
-		
+		int button_width = 150;
+		int button_height = 50;
+
 		/**
 		 * Constructor made to grab pause and reset buttons to place under text
 		 * 
 		 * @author Jose Tua Colon
 		 */
-		public Comp(JButton pause, JButton reset) {
+		public Comp(JButton pause, JButton reset, Timer timer) {
 			this.pause_b = pause;
 			this.reset_b = reset;
-		}
-		/**
-		 * Resets the timer
-		 * 
-		 * @author Gabriel Soto Ramos
-		 */
-		public void reset() {
-			t = System.currentTimeMillis();
-			pT = 0;
-			pause_time = 0;
-		}
-
-		/**
-		 * Gets current miliseconds
-		 * 
-		 * @author Gabriel Soto Ramos
-		 * @return long: Current miliseconds
-		 */
-		public long getMilis() {
-			if (pause)
-				pause_time = System.currentTimeMillis() - pT;
-			long time = System.currentTimeMillis() - pause_time - t;
-			if (time <= 0) {
-				time = 0;
-				reset();
-			}
-			return time;
-		}
-
-		/**
-		 * Pauses the timer
-		 * 
-		 * @author Gabriel Soto Ramos
-		 * @param button : The button that needs it's text changed from Pause to Continue and vice versa
-		 */
-		public void pause(JButton button) {
-			if (!pause) {
-				button.setText("Continue");
-				pause = true;
-				pT = System.currentTimeMillis();
-			} else {
-				button.setText("Pause");
-				pause = false;
-			}
-		}
-
-		/**
-		 * Converts an int to a strings and adds a number of zeroes before it
-		 * 
-		 * @author Gabriel Soto Ramos
-		 * @param n : The number to add the zeroes to
-		 * @param zeroes : Number of zeroes before the number
-		 * @return String
-		 */
-		public String fillWithZeroes(int n, int zeroes) {
-			String number = n + "";
-			int len = number.length();
-			int dl = zeroes - len;
-			if (dl > 0) {
-				for (int i = 0; i < dl; i++) {
-					number = "0" + number;
-				}
-			}
-			return number;
-
+			this.timer = timer;
 		}
 
 		/**
@@ -109,33 +43,24 @@ public class Main {
 		@Override
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
-			ms = getMilis();
-			s = (int) Math.floor(ms / 1000.0);
-			mins = (int) Math.floor(s / 60.0);
-			hrs = (int) Math.floor(mins / 60.0);
-
-			ms = ms % 1000;
-			s = s % 60;
-			mins = mins % 60;
-			hrs = hrs % 100;
-
-			String miliseconds = fillWithZeroes((int) ms, 3);
-			String secs = fillWithZeroes(s, 2);
-			String minutes = fillWithZeroes(mins, 2);
-			String hours = fillWithZeroes(hrs, 3);
-
+			timer.update();
+			if(timer.isPaused())
+				pause_b.setText("Run");
+			else
+				pause_b.setText("Pause");
 			g.setFont(bigFont);
-			
-			timer = hours + " : " + minutes + " : " + secs + " : " + miliseconds;
-			timerText = "Timer Time";
-			double xc = g.getFontMetrics().stringWidth(timer) / 2.0;
-			double yc = g.getFontMetrics().getHeight() / 2.0;
-			g.drawString(timer, (int) (this.getWidth() / 2.0 - xc), (int) (this.getHeight() / 2.0 + yc));
-			g.drawString(timerText, (int) (this.getWidth() / 2.0 - xc + 35), (int) (this.getHeight() / 2.0 - yc - 5));
-			pause_b.setBounds((int) (this.getWidth() / 2.0 - xc) - 20, (int) (this.getHeight()/ 2.0 + yc) + 30, 128, 64);
-			reset_b.setBounds((int) (this.getWidth() / 2.0 - xc) + 200, (int) (this.getHeight()/ 2.0 + yc) + 30, 128, 64);
-			
-			g.drawRect((int) (this.getWidth() / 2.0 - xc) - 5, (int) (this.getHeight() / 2.0 - yc), 260, 50);
+			timer_text = timer.getTimer();
+			String timer_upper_text = "Timer Time";
+			double timer_hwidth = g.getFontMetrics().stringWidth(timer_text) / 2.0;
+			double timerText_hwidth = g.getFontMetrics().stringWidth(timer_upper_text) / 2.0;
+			double text_hheight = g.getFontMetrics().getHeight() / 2.0;
+			g.drawString(timer_text, (int) (this.getWidth() / 2.0 - timer_hwidth), (int) (this.getHeight() / 2.0 + text_hheight));
+			g.drawString(timer_upper_text, (int) (this.getWidth() / 2.0 - timerText_hwidth), (int) (this.getHeight() / 2.0 - text_hheight));
+			pause_b.setBounds((int) (this.getWidth() / 2.0 - timer_hwidth) - button_width/2, (int) (this.getHeight() / 2.0 + text_hheight) + 30, button_width,
+					button_height);
+			reset_b.setBounds((int) (this.getWidth() / 2.0 + timer_hwidth) - button_width/2, (int) (this.getHeight() / 2.0 + text_hheight) + 30, button_width,
+					button_height);
+			g.drawRect((int) (this.getWidth() / 2.0 - timer_hwidth), (int) (this.getHeight() / 2.0 - text_hheight), (int) (timer_hwidth*2), button_height);
 		}
 
 	}
@@ -145,14 +70,15 @@ public class Main {
 		JFrame frame = new JFrame();
 		JButton pause_button = new JButton();
 		JButton reset_button = new JButton();
-		JComponent comp = new Comp(pause_button, reset_button);
+		Timer timer = new Timer();
+		JComponent comp = new Comp(pause_button, reset_button, timer);
 
 		pause_button.addActionListener(new AbstractAction() {
 			private static final long serialVersionUID = 1L;
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				((Comp) comp).pause(pause_button);
+				timer.pause();
 			}
 
 		});
@@ -162,12 +88,10 @@ public class Main {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				((Comp) comp).reset();
+				timer.reset();
 			}
 
 		});
-		pause_button.setBounds(200, 200, 128, 64);
-		reset_button.setBounds(400, 200, 128, 64);
 		pause_button.setText("Start");
 		reset_button.setText("Reset");
 		frame.setTitle("Timer");
---
+++ ICOM4009_Timer/src/Timer.java
@@ -0,0 +1,178 @@
+/**
+ *
+ * A simple timer that can be paused and resetted
+ * 
+ * @author Gabriel Soto Ramos
+ *
+ */
+public class Timer {
+
+	private long t = System.currentTimeMillis();
+	private long pT = 0;
+	private long pause_time = 0;
+	private String timer = "";
+	private int mins = 0;
+	private long ms = 0;
+	private int s = 0;
+	private int hrs = 0;
+	private boolean pause = true;
+
+	/**
+	 * Resets the timer back to 0
+	 * 
+	 * @author Gabriel Soto Ramos
+	 */
+	public void reset() {
+		t = System.currentTimeMillis();
+		pT = 0;
+		pause_time = 0;
+	}
+
+	/**
+	 * Gets current milliseconds
+	 * 
+	 * @author Gabriel Soto Ramos
+	 * @return long: Current milliseconds
+	 */
+	public long getMillis() {
+		if (pause)
+			pause_time = System.currentTimeMillis() - pT;
+		long time = System.currentTimeMillis() - pause_time - t;
+		if (time <= 0) {
+			time = 0;
+			reset();
+		}
+		return time;
+	}
+	
+	/**
+	 * Gets current seconds
+	 * 
+	 * @author Gabriel Soto Ramos
+	 * @return int: Current seconds
+	 */
+	public int getSeconds() {
+		return (int) Math.floor(this.getMillis()/1000)%60;
+	}
+
+	/**
+	 * Gets current minutes
+	 * 
+	 * @author Gabriel Soto Ramos
+	 * @return int: Current minutes
+	 */
+	public int getMinutes() {
+		return (int) Math.floor(this.getSeconds()/60)%60;
+	}
+
+	/**
+	 * Gets current hours
+	 * 
+	 * @author Gabriel Soto Ramos
+	 * @return int: Current hours
+	 */
+	public int getHours() {
+		return (int) Math.floor(this.getMinutes()/60)%24;
+	}
+
+	/**
+	 * Pauses the timer
+	 * 
+	 * @author Gabriel Soto Ramos
+	 * @param button
+	 *            : The button that needs it's text changed from Pause to Continue
+	 *            and vice versa
+	 */
+	public void pause() {
+		if (!pause) {
+			pause = true;
+			pT = System.currentTimeMillis();
+		} else {
+			pause = false;
+		}
+	}
+
+	/**
+	 * Converts an int to a strings and adds a number of zeroes before it
+	 * 
+	 * @author Gabriel Soto Ramos
+	 * @param n
+	 *            : The number to add the zeroes to
+	 * @param zeroes
+	 *            : Number of zeroes before the number
+	 * @return String
+	 */
+	private String fillWithZeroes(int n, int zeroes) {
+		String number = n + "";
+		int len = number.length();
+		int dl = zeroes - len;
+		if (dl > 0) {
+			for (int i = 0; i < dl; i++) {
+				number = "0" + number;
+			}
+		}
+		return number;
+
+	}
+	
+	/**
+	 * Updates the timer
+	 * 
+	 * @author Gabriel Soto Ramos
+	 */
+	public void update() {
+		ms = getMillis();
+		s = (int) Math.floor(ms / 1000.0);
+		mins = (int) Math.floor(s / 60.0);
+		hrs = (int) Math.floor(mins / 60.0);
+
+		ms = ms % 1000;
+		s = s % 60;
+		mins = mins % 60;
+		hrs = hrs % 24;
+
+		String milliseconds = fillWithZeroes((int) ms, 3);
+		String secs = fillWithZeroes(s, 2);
+		String minutes = fillWithZeroes(mins, 2);
+		String hours = fillWithZeroes(hrs, 2);
+
+		timer = hours + " : " + minutes + " : " + secs + " : " + milliseconds;
+	}
+
+	/**
+	 * Gets the timer in hrs:mins:secs:ms
+	 * 
+	 * @author Gabriel Soto Ramos
+	 * @return String
+	 */
+	public String getTimer() {
+		this.update();
+		return timer;
+	}
+
+	/**
+	 * Gets the timer in hrs:mins:secs:ms
+	 * 
+	 * @author Gabriel Soto Ramos
+	 * @param update
+	 *            : Change this to false or leave it blank to get the timer without
+	 *            updating it
+	 * @return String
+	 */
+	public String getTimer(boolean update) {
+		if (update)
+			this.update();
+		return timer;
+	}
+
+	/**
+	 * Check if the timer is paused
+	 * 
+	 * @author Gabriel Soto Ramos
+	 * @return boolean
+	 */
+	public boolean isPaused() {
+		return pause;
+	}
+
+}
