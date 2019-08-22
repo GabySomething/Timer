d3bccbe7e4bcb4ac7f4364b0427366acd4ca3a7c
Bernardo2207 <33329311+Bernardo2207@users.noreply.github.com>
2019-08-22 11:30:20 -0400

    Se agrego colores distintos a la barra de progreso de los segunods.

--- ICOM4009_Timer/src/Main.java
+++ ICOM4009_Timer/src/Main.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
@@ -9,7 +10,7 @@ import javax.swing.JFrame;
 public class Main {
 
 	public static class Comp extends JComponent {
-		
+
 		/**
 		 * 
 		 */
@@ -39,28 +40,99 @@ public class Main {
 		 * 
 		 * @author Gabriel Soto Ramos
 		 * @author Jose Tua Colon
+		 * @author Mario Rodriguez
 		 */
 		@Override
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
+			g.setColor(Color.DARK_GRAY);
+			g.fillRect(0, 0, this.getWidth(), this.getHeight());
+			g.setColor(Color.WHITE);
 			timer.update();
-			if(timer.isPaused())
+			if (timer.isPaused())
 				pause_b.setText("Run");
 			else
 				pause_b.setText("Pause");
 			g.setFont(bigFont);
 			timer_text = timer.getTimer();
-			String timer_upper_text = "Timer Time";
+			
+			
 			double timer_hwidth = g.getFontMetrics().stringWidth(timer_text) / 2.0;
-			double timerText_hwidth = g.getFontMetrics().stringWidth(timer_upper_text) / 2.0;
+						
 			double text_hheight = g.getFontMetrics().getHeight() / 2.0;
-			g.drawString(timer_text, (int) (this.getWidth() / 2.0 - timer_hwidth), (int) (this.getHeight() / 2.0 + text_hheight));
-			g.drawString(timer_upper_text, (int) (this.getWidth() / 2.0 - timerText_hwidth), (int) (this.getHeight() / 2.0 - text_hheight));
-			pause_b.setBounds((int) (this.getWidth() / 2.0 - timer_hwidth) - button_width/2, (int) (this.getHeight() / 2.0 + text_hheight) + 30, button_width,
-					button_height);
-			reset_b.setBounds((int) (this.getWidth() / 2.0 + timer_hwidth) - button_width/2, (int) (this.getHeight() / 2.0 + text_hheight) + 30, button_width,
-					button_height);
-			g.drawRect((int) (this.getWidth() / 2.0 - timer_hwidth), (int) (this.getHeight() / 2.0 - text_hheight), (int) (timer_hwidth*2), button_height);
+			g.drawString(timer_text, (int) (this.getWidth() / 2.0 - timer_hwidth),
+					(int) (this.getHeight() / 2.0 + text_hheight));
+			
+			pause_b.setBounds((int) (this.getWidth() / 2.0 - timer_hwidth) - button_width / 2,
+					(int) (this.getHeight() / 2.0 + text_hheight) + 30, button_width, button_height);
+			reset_b.setBounds((int) (this.getWidth() / 2.0 + timer_hwidth) - button_width / 2,
+					(int) (this.getHeight() / 2.0 + text_hheight) + 30, button_width, button_height);
+//			g.drawRect((int) (this.getWidth() / 2.0 - timer_hwidth), (int) (this.getHeight() / 2.0 - text_hheight),
+//					(int) (timer_hwidth * 2), button_height);
+			
+			int wbox= g.getFontMetrics().stringWidth("00");
+			int wmbox= g.getFontMetrics().stringWidth("000");
+			int wboxborder= g.getFontMetrics().stringWidth(" : ");
+			
+			g.setFont(smallFont);
+			String timer_upper_text = "Timer Time";
+			double timerText_hwidth = g.getFontMetrics().stringWidth(timer_upper_text) / 2.0;
+
+//			g.drawString(timer_upper_text, (int) (this.getWidth() / 2.0 - timerText_hwidth),
+//					(int) (this.getHeight() / 2.0 - text_hheight));
+			
+			String tHours= "Hrs";
+			String tMinutes= "Mins";
+			String tSeconds= "Secs";
+			String tMilliseconds= "Millis";
+			//Component Width
+			int compWith= this.getWidth();
+			//Components Height
+			int compHeight = this.getHeight();
+			
+			int wtHours= g.getFontMetrics().stringWidth(tHours);
+			int wtMinutes= g.getFontMetrics().stringWidth(tMinutes);
+			int wtSeconds= g.getFontMetrics().stringWidth(tSeconds);
+			int wtMilliseconds= g.getFontMetrics().stringWidth(tMilliseconds);
+			
+			g.drawString(tHours, (int)((compWith+wbox-wtHours)/2-timer_hwidth), (int) (compHeight/2-text_hheight));
+			g.drawString(tMinutes, (int)((compWith-wtMinutes+wtHours)/2-timer_hwidth+  wboxborder+ wbox), (int) (compHeight/2-text_hheight));
+			g.drawString(tSeconds, (int)((compWith-wtSeconds+wtHours+wtMinutes)/2-timer_hwidth+  wboxborder+ 2*wbox+10), (int) (compHeight/2-text_hheight));
+			g.drawString(tMilliseconds, (int)((compWith/2+timer_hwidth-wtMilliseconds/2-wmbox/2)), (int) (compHeight/2-text_hheight));
+			
+			
+			for (int i = 0; i < timer.getSeconds() % 10; i++) {
+
+			
+				
+				int w = 50, h = 50;
+				
+				int x= this.getWidth()-w;
+				int y= (9-i)*h;
+				
+				
+				
+				if( i>=0 && i<3) {
+				g.setColor(Color.GREEN);}
+				
+				else if(i>=3 && i<6) {
+					g.setColor(Color.YELLOW);
+				}
+				
+				else {
+					g.setColor(Color.RED);
+					
+				}
+				g.fillRect(x, y, w, h);
+				
+				g.setColor(Color.DARK_GRAY);
+				g.drawRect(x, y, w, h);
+				
+				
+				
+
+			}
+
 		}
 
 	}
@@ -68,6 +140,7 @@ public class Main {
 	public static void main(String[] args) {
 		int frameWidth = 800, frameHeight = 600;
 		JFrame frame = new JFrame();
+		frame.setResizable(false);
 		JButton pause_button = new JButton();
 		JButton reset_button = new JButton();
 		Timer timer = new Timer();
